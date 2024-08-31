37ad6b061f776e738a02a31b778332b31cba4985
Ivan-developer-cmngsoon <ivanwork003@gmail.com>
2024-08-31 03:56:09 -0400

    Añadiendo detalles a paginas

--- AppBefine/src/app/login/login-routing.module.ts
+++ AppBefine/src/app/login/login-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './login.page';
+import { HomePage } from '../home/home.page';
+import { PedidosPage } from '../pedidos/pedidos.page';
+import { BodegaPage } from '../bodega/bodega.page';
 
 const routes: Routes = [
+  { path: 'home', component: HomePage },
+  { path: 'pedidos', component: PedidosPage },
+  { path: 'bodega', component: BodegaPage },
+
   {
     path: '',
     component: LoginPage
