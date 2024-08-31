c5ac0295f566ac7d05317b457f8a2627f7b07c92
Cmfg96 <CAR.FUENTESG@duocuc.cl>
2024-08-30 21:31:31 -0400

    enrutamiento

--- AppBefine/src/app/login/login.page.ts
+++ AppBefine/src/app/login/login.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  // Método para manejar el enrutamiento después de un clic en el botón de login
+  onLogin() {
+    // Redirige directamente a la página de inicio (home)
+    this.navCtrl.navigateForward('/home');
   }
-
 }
