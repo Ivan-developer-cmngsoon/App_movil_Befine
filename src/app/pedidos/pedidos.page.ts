356469f82481303ca94480f2bf52acf2ca20038b
IvanRodriguezPinares <iv.rodriguezp@duocuc.cl>
2024-09-11 01:54:31 -0300

    Validaciones en pedidos.page de Jquery

--- src/app/pedidos/pedidos.page.ts
+++ src/app/pedidos/pedidos.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular'; // Importamos el controlador del modal
+import { ModalController } from '@ionic/angular'; // controlador de modal
 
 @Component({
   selector: 'app-pedidos',
@@ -7,16 +7,36 @@ import { ModalController } from '@ionic/angular'; // Importamos el controlador d
   styleUrls: ['./pedidos.page.scss'],
 })
 export class PedidosPage implements OnInit {
+  showWarningMessage: boolean = false; // Variable para controlar la visualización del mensaje de números
+  showPaymentWarning: boolean = false; // Variable para controlar la visualización del mensaje de medio de pago
 
-  constructor(private modalController: ModalController) { } // Inyectamos el controlador del modal
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() { }
 
-  /**
-   * Método para cerrar el modal
-   * Este método se ejecuta cuando el usuario hace clic en el botón "Enviar" dentro del modal.
-   */
+  // Método para verificar si solo se están ingresando números
+  validateNumbers(event: any) {
+    const value = event.target.value;
+    // Si el valor contiene algo que no sea un número, muestra el mensaje
+    if (isNaN(Number(value))) {
+      this.showWarningMessage = true;
+    } else {
+      this.showWarningMessage = false;
+    }
+  }
+
+  // Método para verificar si el medio de pago es "Tarjeta" o "Efectivo"
+  validatePaymentMethod(event: any) {
+    const value = event.target.value.toLowerCase();
+    // Si el valor no es "tarjeta" ni "efectivo", muestra el mensaje
+    if (value !== 'tarjeta' && value !== 'efectivo') {
+      this.showPaymentWarning = true;
+    } else {
+      this.showPaymentWarning = false;
+    }
+  }
+
   async dismissModal() {
-    await this.modalController.dismiss(); // Cierra el modal
+    await this.modalController.dismiss();
   }
 }
