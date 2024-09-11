204b3d18439cb9526ed352527eedd16f3c944b3c
IvanRodriguezPinares <iv.rodriguezp@duocuc.cl>
2024-09-11 01:21:02 -0300

    Home Bienvenida con animacion Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('welcomeMessage', { read: ElementRef, static: true }) welcomeMessage!: ElementRef;
 
-  constructor() {}
+  constructor(private animationCtrl: AnimationController) {}
 
+  ngAfterViewInit() {
+    // Definir la animación para el mensaje de bienvenida
+    const animation = this.animationCtrl
+      .create()
+      .addElement(this.welcomeMessage.nativeElement)
+      .duration(1500) // Duración de la animación en milisegundos
+      .fromTo('opacity', '0', '1'); // Efecto de desvanecimiento (fade in)
+
+    animation.play(); // Ejecutar la animación
+  }
 }
